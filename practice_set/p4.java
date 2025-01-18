18c1cdc5caa5221eaad7a8c0ab9907c7313ac5cb
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-18 23:38:43 +0530

    added patterns

--- practice_set/p4.java
+++ practice_set/p4.java
@@ -46,19 +46,61 @@
 // }
 
 //floyd's triangle
-import java.util.*;
-public class p4{
-    public static void main(String arg[]){
-        Scanner sc= new Scanner(System.in);
-        int n = 0;
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                n += 1;
-                System.out.print(n);
+// import java.util.*;
+// public class p4{
+//     public static void main(String arg[]){
+//         Scanner sc= new Scanner(System.in);
+//         int n = 0;
+//         for(int i=1;i<=5;i++){
+//             for(int j=1;j<=i;j++){
+//                 n += 1;
+//                 System.out.print(n);
                 
+//             }
+//             System.out.println("");
+//             sc.close();
+//         }
+//     }
+// }
+
+//hollow rectangle
+// public class p4{
+//     public static void hollow_rect(int totRows , int totCols){
+//         //outer loop
+//         for (int i=1;i<=totRows;i++){
+//             //inner loop
+//             for(int j=1;j<=totCols;j++){
+//                 //cell - (i,j)
+//                 if(i==1 || i==totRows || j==1 ||j==totCols){
+//                     System.out.print("*");
+//                 }else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String args[]){
+//         hollow_rect(4,5);
+//     }
+// }
+
+//inverted rotated half-pyramid
+public class p4{
+        public static void irhp(int n){
+            //lines
+            for (int i=1; i<=4;i++){
+                //spaces
+                for (int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for (int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println("");
-            sc.close();
+        }
+        public static void main(String args[]){
+            irhp(4);
         }
     }
-}
