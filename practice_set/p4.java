52e9fb9178563255cbd0493110453534272f2502
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-19 00:55:05 +0530

    added patterns

--- practice_set/p4.java
+++ practice_set/p4.java
@@ -86,21 +86,150 @@
 // }
 
 //inverted rotated half-pyramid
+// public class p4{
+//         public static void irhp(int n){
+//             //lines
+//             for (int i=1; i<=4;i++){
+//                 //spaces
+//                 for (int j=1;j<=n-i;j++){
+//                     System.out.print(" ");
+//                 }
+//                 for (int j=1;j<=i;j++){
+//                     System.out.print("*");
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//         public static void main(String args[]){
+//             irhp(4);
+//         }
+//     }
+
+
+//0-1 triangle
+// public class p4{
+//         public static void irhp(int row){
+//             //lines
+//             for (int i=1; i<=5;i++){
+//                 for(int j=1;j<=i;j++){
+//                     if((i+j)%2==0){
+//                         System.out.print("1");
+//                     }else{
+//                         System.out.print("0");
+//                     }
+//                 }System.out.println("");
+//             }
+//         }
+//         public static void main(String args[]){
+//             irhp(5);
+//         }
+//     }
+
+//butterfly pattern
+// public class p4{
+//             public static void butterfly(int n){
+//                 //lines
+//                 for (int i=1; i<=n;i++){
+//                     for(int j=1;j<=i;j++){
+//                         System.out.print("*");
+//                     }
+//                     for(int j=1;j<=(2*n-2*i);j++){
+//                         System.out.print(" ");
+//                     }
+//                     for(int j=1;j<=i;j++){
+//                         System.out.print("*");
+//                     }
+//                     System.out.println();
+//                 }
+
+//                 for (int i=n; i>=1;i--){
+//                     for(int j=1;j<=i;j++){
+//                         System.out.print("*");
+//                     }
+//                     for(int j=1;j<=(2*n-2*i);j++){
+//                         System.out.print(" ");
+//                     }
+//                     for(int j=1;j<=i;j++){
+//                         System.out.print("*");
+//                     }
+//                     System.out.println();
+//                 }
+//             }
+//             public static void main(String args[]){
+//                 butterfly(4);
+//             }
+//         }
+
+//rhombus
+// public class p4{
+//         public static void rhombus(int row){
+//             //lines
+//             for (int i=1; i<=5;i++){
+//                 for(int j=1;j<=(row-i);j++){
+//                     System.out.print(" ");
+//                 }
+//                 for(int j =1;j<=5;j++){
+//                     System.out.print("*");
+//                 }System.out.println("");
+//             }
+//         }
+//         public static void main(String args[]){
+//             rhombus(5);
+//         }
+//     }
+
+//hollow rhombus
+// public class p4{
+//         public static void rhombus(int n){
+//             //lines
+//             for (int i=1; i<=n;i++){
+//                 //spaces
+//                 for(int j=1;j<=(n-i);j++){
+//                     System.out.print(" ");
+//                 }
+
+//                 //hollow rect
+//                 for(int j=1;j<=n;j++){
+//                     if(i==1||i==n||j==1||j==n){
+//                         System.out.println("*");
+//                     }else{
+//                         System.out.print(" ");
+//                     }
+//                 }System.out.println("");
+//             }
+//         }
+//         public static void main(String args[]){
+//             rhombus(5);
+//     }
+// }
+
+//diamond
 public class p4{
-        public static void irhp(int n){
+        public static void diamond(int n){
             //lines
-            for (int i=1; i<=4;i++){
+            for (int i=1; i<=n;i++){
                 //spaces
-                for (int j=1;j<=n-i;j++){
+                for(int j=1;j<=n-i;j++){
                     System.out.print(" ");
                 }
-                for (int j=1;j<=i;j++){
+                for(int j=1;j<=(2*i)-1;j++){
                     System.out.print("*");
                 }
-                System.out.println();
+                System.out.println("");
+            }
+
+            for (int i=n; i>=1;i--){
+                //spaces
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=(2*i)-1;j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
             }
         }
         public static void main(String args[]){
-            irhp(4);
+            diamond(4);
         }
     }
