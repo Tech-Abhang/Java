72db2f8d20dbf9f4fd59bd09a4fb369add4b4682
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-21 22:15:01 +0530

    created search

--- practice_set/p6.java
+++ practice_set/p6.java
@@ -1,3 +1,23 @@
+import java.util.*;
 public class p6 {
-    
+    public static void main(String args[]){
+        int marks[] = {99,99,99};
+        Scanner sc = new Scanner(System.in);
+        int nonChange = 5;
+        update(marks,nonChange);
+
+        for (int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+ " ");;
+        }
+        System.out.println(nonChange);
+
+    }
+
+    public static void update(int marks[],int nonChange){
+        for (int i=0;i<marks.length;i++){
+            marks[i] = marks[i] +1;
+        }
+        nonChange = 10;
+        
+    }
 }
