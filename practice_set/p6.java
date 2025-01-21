b0e362f4203f076f2edd2ae3aceddde039287599
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-21 23:33:02 +0530

    p6

--- practice_set/p6.java
+++ practice_set/p6.java
@@ -1,23 +1,42 @@
-import java.util.*;
+// import java.util.*;
+// public class p6 {
+//     public static void main(String args[]){
+//         int marks[] = {99,99,99};
+//         Scanner sc = new Scanner(System.in);
+//         int nonChange = 5;
+//         update(marks,nonChange);
+
+//         for (int i=0;i<marks.length;i++){
+//             System.out.print(marks[i]+ " ");;
+//         }
+//         System.out.println(nonChange);
+
+//     }
+
+//     public static void update(int marks[],int nonChange){
+//         for (int i=0;i<marks.length;i++){
+//             marks[i] = marks[i] +1;
+//         }
+//         nonChange = 10;
+        
+//     }
+// }
+
+//find the largest element
 public class p6 {
-    public static void main(String args[]){
-        int marks[] = {99,99,99};
-        Scanner sc = new Scanner(System.in);
-        int nonChange = 5;
-        update(marks,nonChange);
-
-        for (int i=0;i<marks.length;i++){
-            System.out.print(marks[i]+ " ");;
+    public static int getLargest(int numbers[]){
+        int largest = numbers[0];
+        for (int i=1;i<numbers.length;i++){
+            if (numbers[i]>largest){
+                largest = numbers[i];
+            }
         }
-        System.out.println(nonChange);
-
+        return largest;
     }
 
-    public static void update(int marks[],int nonChange){
-        for (int i=0;i<marks.length;i++){
-            marks[i] = marks[i] +1;
-        }
-        nonChange = 10;
-        
+    public static void main(String args[]){
+        int numbers[] = {1,2,6,4,5};
+        int largestNum= getLargest(numbers);
+        System.out.println(largestNum);
     }
 }
--- search/linear.java
+++ search/linear.java
@@ -1,5 +1,24 @@
 package search;
 
 public class linear {
-    
+    public static int linearSearch(String[] names, String key){
+        for (int i =0 ; i<names.length;i++){
+            if(names[i].equals(key)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        String[] names = {"abhang","shubham","aryan"};
+        String key = "abhang";
+        int index = linearSearch(names,key);
+        if(index == -1){
+            System.out.println("key not found");
+        }else{
+            System.out.println("key is at"+ index);
+        }
+        
+    }
 }
