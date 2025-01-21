c9676af4161ac9d887e4e65c91336c3a43dc502f
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-22 00:21:18 +0530

    reverse array

--- practice_set/p6.java
+++ practice_set/p6.java
@@ -23,20 +23,58 @@
 // }
 
 //find the largest element
+// public class p6 {
+//     public static int getLargest(int numbers[]){
+//         int largest = numbers[0];
+//         for (int i=1;i<numbers.length;i++){
+//             if (numbers[i]>largest){
+//                 largest = numbers[i];
+//             }
+//         }
+//         return largest;
+//     }
+
+//     public static void main(String args[]){
+//         int numbers[] = {1,2,6,4,5};
+//         int largestNum= getLargest(numbers);
+//         System.out.println(largestNum);
+//     }
+// }
+
+//reverse
+// public class p6 {
+//     public static void reverse(int numbers[]){
+//         int start = 0 ; int end = numbers.length-1;
+//         while (start < end){
+//             int temp = numbers[start];
+//             numbers[start] = numbers[end];
+//             numbers[end] = temp;
+//             start = start +1;
+//             end = end -1;
+//         }
+//     }
+
+//     public static void main(String args[]){
+//         int numbers[] = {2,4,6,8,10};
+//         reverse(numbers);
+//         for (int i=0;i<numbers.length;i++){
+//             System.out.print(numbers[i]+ " ");
+//         }
+        
+//     }
+// }
+
+//pairs of array
 public class p6 {
-    public static int getLargest(int numbers[]){
-        int largest = numbers[0];
-        for (int i=1;i<numbers.length;i++){
-            if (numbers[i]>largest){
-                largest = numbers[i];
-            }
-        }
-        return largest;
-    }
+
 
     public static void main(String args[]){
-        int numbers[] = {1,2,6,4,5};
-        int largestNum= getLargest(numbers);
-        System.out.println(largestNum);
+        int numbers[] = {2,4,6,8,10};
+        for (int i=0;i<numbers.length;i++){
+            for (int j=i;j<numbers.length;j++){
+                System.out.println("("+numbers[i]+","+numbers[j]+")");
+            }
+        }
+        
     }
 }
