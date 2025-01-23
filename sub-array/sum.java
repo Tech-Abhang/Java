c4d1a4be9cf244743676ee0982c1a2a2a96982f1
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-23 22:16:25 +0530

    added sum.java

---
+++ sub-array/sum.java
@@ -0,0 +1,21 @@
+//brute force
+
+public class sum {
+    public static void main(String args[]){
+        int maxSum = 0;
+        int arr[] = {2,4,6,8,10};
+        for (int i =0 ; i<arr.length;i++){
+            for (int j = i;j<arr.length;j++){
+                int currSum = 0 ;
+                for (int k = i ; k<=j;k++){
+                    currSum = currSum + arr[k]; 
+                }
+                System.out.println(currSum);
+                if(maxSum<currSum){
+                    maxSum = currSum ; 
+                }
+            }System.out.println();
+        }
+        System.out.println("max sum is : "+maxSum);
+    }
+}
