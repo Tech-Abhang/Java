d112e3e89f75467824e170f640ef2f912ffe4cd9
Abhang Pawar <abhangpawar03@gmail.com>
2025-01-24 19:46:55 +0530

    dsa question

---
+++ DSA/stock.java
@@ -0,0 +1,19 @@
+package DSA;
+
+public class stock {
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0 ; i<prices.length ; i++){
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice ; 
+                maxProfit = Math.max(maxProfit , profit);
+            }else{
+                buyPrice = prices[i];
+            }
+        }
+        System.out.println(maxProfit);
+    }
+}
