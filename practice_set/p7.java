1ebaa708f598945f3e950a896e1565d2d0ca12f9
Abhang Pawar <abhangpawar03@gmail.com>
2025-02-04 18:33:09 +0530

    Q1

---
+++ practice_set/p7.java
@@ -0,0 +1,17 @@
+//bitwise operators
+
+//get ith bit
+import java.util.*;
+public class p7 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num = sc.nextInt();
+            int bitnum = 1;
+            int i = sc.nextInt();
+            if((num & (bitnum<<i)) == 0){
+                System.out.println(0);
+            }else{
+                System.out.println(1);
+            }
+        }
+}
