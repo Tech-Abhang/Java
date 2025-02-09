4e2f6da9a2e1fbaa0bfd90858de6bf44e680181f
Abhang Pawar <abhangpawar03@gmail.com>
2025-02-09 15:11:51 +0530

    added questions

--- recursion/practice_re.java
+++ recursion/practice_re.java
@@ -2,6 +2,8 @@
 package recursion;
 
 public class practice_re {
+
+    //print n to 1 
     public static void printnum(int n){
         //base case to avoild overlowing
         if(n == 1){
@@ -13,8 +15,26 @@ public class practice_re {
         printnum(n-1);
     }
 
+    //print 1 to n
+    public static void inc(int n) {
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        inc(n-1);
+        System.out.println(n);
+
+    }
+
+    //sum n natrual numbers
+    public static int sumofn(int n) {
+        if(n==1){
+            return 1;
+        }
+        return n + sumofn(n-1);
+    }
+
     public static void main(String args[]){
-        int i = 10 ; 
-        printnum(i);
+        int i = 10 ;
     }
 }
